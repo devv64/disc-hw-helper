90e713d1522589e5b7e4e9411b502369f7a914d8
Dev <devextraemail@gmail.com>
2023-08-03 00:45:40 -0400

    Docstrings/Comments

--- utils/commands.py
+++ utils/commands.py
@@ -10,12 +10,25 @@ import asyncio
 headers = {'Authorization': f'Bearer {CANVAS_ACCESS_TOKEN}'}
 
 def setup_commands(bot):
+    """
+    Set up custom bot commands.
+
+    Args:
+        bot (commands.Bot): The bot instance to set up commands for.
+    """
+        
     @bot.command()
     async def helloworld(ctx):
         await ctx.send("Hello World!")
 
     @bot.command()
     async def homework(ctx):
+        """
+        Retrieves and displays homework assignments from Canvas.
+
+        Args:
+            ctx (commands.Context): The context in which the command is called.
+        """
         now = datetime.datetime.now()
         min_date = datetime.datetime.now() - datetime.timedelta(days=260)
         assignments = get_homework_assignments(min_date.date())
@@ -39,6 +52,15 @@ def setup_commands(bot):
         print(datetime.datetime.now() - now)
 
     def get_homework_assignments(min_date):
+        """
+        Retrieve filtered homework assignments based on the minimum due date.
+
+        Args:
+            min_date (datetime.date): The minimum due date to consider.
+
+        Returns:
+            list: List of filtered assignment details.
+        """
         filtered_assignments = []
 
         for course in course_cache:
@@ -63,6 +85,12 @@ def setup_commands(bot):
         return filtered_assignments
 
     def fetch_courses():
+        """
+        Fetches the list of courses from the Canvas API.
+
+        Returns:
+            list: List of courses retrieved from the API.
+        """
         courses_url = f'{CANVAS_API_URL}/courses'
         response = requests.get(courses_url, headers=headers, verify=certifi.where())
 
@@ -72,6 +100,15 @@ def setup_commands(bot):
             return []
 
     def fetch_assignments(course_id):
+        """
+        Fetches assignments for a specific course from the Canvas API.
+
+        Args:
+            course_id (int): The ID of the course to fetch assignments for.
+
+        Returns:
+            list: List of assignments retrieved from the API for the specified course.
+        """
         assignments_url = f'{CANVAS_API_URL}/courses/{course_id}/assignments'
         response = requests.get(assignments_url, headers=headers, verify=certifi.where())
 
@@ -81,6 +118,12 @@ def setup_commands(bot):
             return []
 
     def start_reminder_task():
+        """
+        Starts the reminder task for upcoming assignments.
+
+        This function simulates and adds upcoming assignments to the assignment cache,
+        then starts the remind_upcoming_assignments background task.
+        """        
         global reminder_task
         if reminder_task and not reminder_task.is_cancelled():
             return
@@ -107,7 +150,7 @@ def setup_commands(bot):
         },
         {
             'title': 'Assignment 4',
-            'due_date': datetime.datetime.now() + datetime.timedelta(hours=12, seconds=10),  # 12 hours from now
+            'due_date': datetime.datetime.now() + datetime.timedelta(hours=12, seconds=60),  # 12 hours from now
             'status': 'published',
             'course': 'Course D',
         }
@@ -120,10 +163,15 @@ def setup_commands(bot):
 
         reminder_task = remind_upcoming_assignments.start()
 
-    @tasks.loop(seconds=10)
+    @tasks.loop(seconds=60)
     async def remind_upcoming_assignments():
+        """
+        This asynchronous function is a background task that periodically checks
+        if there are any upcoming assignments and sends reminders if necessary.
+        """
         now = datetime.datetime.now()
 
+        # Define the intervals at which reminders should be sent
         reminder_intervals = [
             (5, "5 minutes"),
             (30, "30 minutes"),
@@ -143,6 +191,7 @@ def setup_commands(bot):
                 time_difference = due_date - now
                 time_difference_minutes = time_difference.total_seconds() / 60
 
+                # Iterate over reminder intervals and check if it's time to send a reminder
                 for interval, interval_name in reminder_intervals:
                     if 0 <= time_difference_minutes <= interval:
                         # Check if this assignment has already been reminded for this interval
@@ -151,10 +200,21 @@ def setup_commands(bot):
                             channel = bot.get_channel(1127058724754821241)
                             message = f"Reminder: The assignment '{assignment.get('title')}' is due in {interval_name}!"
                             await channel.send(message)
-                            assignment[f'reminded_{interval_name}'] = True  # Mark as reminded
+
+                            # Mark this assignment as reminded for this interval
+                            assignment[f'reminded_{interval_name}'] = True
                         break
 
     def get_course_by_id(course_id):
+        """
+        Get course details by its ID.
+
+        Args:
+            course_id (int): The ID of the course to retrieve details for.
+
+        Returns:
+            dict or None: Course details if found, otherwise None.
+        """    
         for course in course_cache:
             if course.get('id') == course_id:
                 return course
@@ -162,7 +222,6 @@ def setup_commands(bot):
 
     # Fetch courses and assignments when the bot starts up
     course_cache = fetch_courses()
-
     for course in course_cache:
         course_id = course.get('id')
         assignments = fetch_assignments(course_id)
